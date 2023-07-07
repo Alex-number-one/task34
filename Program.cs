cbc40feca388a8f988bdca16a66169898e210b04
Alex <aleksejtatukov098@gmail.com>
2023-07-07 22:40:21 +0300

    task35

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int size = new Random().Next(1, 10);
+int[] array = new int[size];
+  int j = 1;
+  int sum = 0;
+for (int i = 0; i < size; i++)
+{
+   
+    
+    array[i] = new Random().Next(1, 10);
+    if (j % 2 != 0)
+    {
+       sum += array[i];
+        
+    }
+      j++;
+  Console.Write( array[i] + " ");
+  
+}
+Console.Write("-> " + sum);
+
+
