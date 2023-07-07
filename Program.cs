2b2bf5d2ddf04e01b6477e7a01f323153114c82b
Alex <aleksejtatukov098@gmail.com>
2023-07-07 23:38:37 +0300

    task38

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int size = 6;
+int [] array = new int [size];
+int min = 11;
+int max = 0;
+for (int  i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(1 , 10);
+     
+    
+if (array[i] < min)
+{
+    min = array[i];
+}
+if (array[i] > max)
+{
+    max = array[i];
+}
+
+
+
+
+     Console.Write(array[i] + " ");
+}
+Console.Write($" -> {max - min}");
